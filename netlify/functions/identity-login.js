efc71b74e19c7e9108bd350d4d189153f3d0e815
JSman225 <93222543+KarlLeonardi@users.noreply.github.com>
2022-05-04 15:20:01 -0400

    Update identity-login.js

--- netlify/functions/identity-login.js
+++ netlify/functions/identity-login.js
@@ -14,6 +14,7 @@ exports.handler = function(event, context, callback) {
   };
   callback(null, {
     statusCode: 200,
-    body: /*JSON.stringify*/console.log(responseBody)
+    body: JSON.stringify(responseBody)
   });
 };
+console.log(responseBody)
