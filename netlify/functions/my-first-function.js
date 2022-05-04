9a0834658bfca53cafa9bed6d520496573109df2
JSman225 <93222543+KarlLeonardi@users.noreply.github.com>
2022-05-04 18:51:51 -0400

    Update my-first-function.js

--- netlify/functions/my-first-function.js
+++ netlify/functions/my-first-function.js
@@ -1,4 +1,17 @@
 exports.handler = async () => ({
+    const data = JSON.parse(event.body);
+  const { user } = data;
+    
+  const responseBody = {
+    app_metadata: {
+      roles: ["visitor"],
+      my_user_info: "this is some user info"
+    },
+    user_metadata: {
+      ...user.user_metadata, // append current user metadata
+      custom_data_from_function: "hurray this is some extra metadata"
+    }
+  };
   statusCode: 200,
-  body: 'boop',
+  body: JSON.stringify(responseBody),
 });
