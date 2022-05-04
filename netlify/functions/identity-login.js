16767f23afdf34e0373c5b118b3a88da42370a2c
JSman225 <93222543+KarlLeonardi@users.noreply.github.com>
2022-05-04 15:22:57 -0400

    test of JSON thing i duno lmao

--- netlify/functions/identity-login.js
+++ netlify/functions/identity-login.js
@@ -1,20 +1,6 @@
-exports.handler = function(event, context, callback) {
-  const data = JSON.parse(event.body);
-  const { user } = data;
-    
-  const responseBody = {
-    app_metadata: {
-      roles: ["visitor"],
-      my_user_info: "this is some user info"
-    },
-    user_metadata: {
-      ...user.user_metadata, // append current user metadata
-      custom_data_from_function: "hurray this is some extra metadata"
-    }
-  };
-  callback(null, {
+exports.handler = async function (event, context) {
+  return {
     statusCode: 200,
-    body: JSON.stringify(responseBody)
-  });
-};
-console.log(responseBody)
+    body: JSON.stringify({ message: "Hello World" }),
+  };
+}
