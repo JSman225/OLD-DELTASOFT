6c9b8b00e6a80886b4823ff1da15f65970f11072
JSman225 <93222543+KarlLeonardi@users.noreply.github.com>
2022-05-06 12:51:30 -0400

    Update js.js

--- js.js
+++ js.js
@@ -179,6 +179,6 @@ netlifyIdentity.on('logout', () => {
 }
 var profileImage;
 function lmao(){
-   netlifyIdentity.currentUser().app_metadata.role = ['test1','test2'];
+   netlifyIdentity.currentUser().app_metadata.roles = ['test1','test2'];
   
 }
