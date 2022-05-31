807f4197961f1ba02be5c49ab0c2a7546e264a3a
JSman225 <93222543+KarlLeonardi@users.noreply.github.com>
2022-05-31 11:09:39 -0400

    Update test.js

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,7 +1,18 @@
-exports.handler = function(event, context, callback) {
+/*exports.handler = function(event, context, callback) {
+  
    test = "this is a test"
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(test)
   });
-};
+};*/
+ //parameter
+  exports.handler = async event => {
+      const querystring = event.queryStringParameters;
+     const boopee = querystring.boopee || 'a friend';
+     
+         return {
+      statusCode: 200,
+      body: `You booped ${boopee} on the nose. Boop!`,
+    };
+  };
