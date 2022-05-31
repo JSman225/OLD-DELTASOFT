9ae8bcfc48df8027861d416afd32da9e33374cef
JSman225 <93222543+KarlLeonardi@users.noreply.github.com>
2022-05-31 10:50:58 -0400

    Update test.js

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,19 +1,7 @@
 exports.handler = function(event, context, callback) {
-  const data = JSON.parse(event.body);
-  const { user } = netlifyIdentity.currentUser;
-    
-  const responseBody = {
-    app_metadata: {
-      roles: ["visitor"],
-      my_user_info: "this is some user info"
-    },
-    user_metadata: {
-      ...user.user_metadata, // append current user metadata
-      custom_data_from_function: "hurray this is some extra metadata"
-    }
-  };
+   test = "this is a test"
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify(responseBody)
+    body: JSON.stringify(test)
   });
 };
